6981d33e1972a9da2272e793c68ec4239a0ba87b
naivedyamishra9999 <naivedyamishra9999@gmail.com>
2026-02-26 23:14:21 +0530

    Add files via upload

---
+++ code26.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure for Doubly Linked List Node
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+// Function to insert node at the end
+struct Node* insertEnd(struct Node* head, int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    // If list is empty
+    if (head == NULL) {
+        return newNode;
+    }
+
+    struct Node* temp = head;
+
+    // Traverse to last node
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    // Insert at end
+    temp->next = newNode;
+    newNode->prev = temp;
+
+    return head;
+}
+
+// Function to traverse and print list forward
+void traverseForward(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, value;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        head = insertEnd(head, value);
+    }
+
+    traverseForward(head);
+
+    return 0;
+}
